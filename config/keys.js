35a346a5d4ec2b805b7dda636ac0e2335fd33c0f
Robert Shaw <robertshaw87@gmail.com>
2018-06-11 14:41:48 -0700

    baby steps toward oauth

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 var keys = module.exports = {};
 
 keys.googleOAuth = {
-    clientID: process.env.GOOGLE_OAUTH_KEY,
+    clientID: process.env.GOOGLE_OAUTH_CLIENTID,
     clientSecret: process.env.GOOGLE_OAUTH_SECRET
 }
