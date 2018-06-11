2653ad9e3aed415e3f01f3e60451679aa7b505ab
Robert Shaw <robertshaw87@gmail.com>
2018-06-11 10:41:47 -0700

    add initial controller for rendering handlebars

--- controller/media_controller.js
+++ controller/media_controller.js
@@ -1,1 +1,15 @@
 var db = require("../models");
+
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.render("index");
+    });
+
+    app.get("/login", function (req, res) {
+        res.render("login");
+    });
+
+    app.get("/recommendations", function (req, res) {
+        res.render("recommendations");
+    });
+}
